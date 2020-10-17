19a78ce814521e6bc9711232a4ddc9f905eae37c
deepmal933 <deepmal944@gmail.com>
2020-10-17 11:14:29 -0400

    stream the video from webcam

--- src/App.js
+++ src/App.js
@@ -3,22 +3,37 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const mediaStreamConstraints = {
+    video: true,
+  };
+  
+  
+  // Local stream that will be reproduced on the video.
+  let localStream;
+  
+  // Handles success by adding the MediaStream to the video element.
+  function gotLocalMediaStream(mediaStream) {
+    const localVideo = document.querySelector('video');
+
+    localStream = mediaStream;
+    localVideo.srcObject = mediaStream;
+  }
+  
+  // Handles error by logging a message to the console with the error message.
+  function handleLocalMediaStreamError(error) {
+    console.log('navigator.getUserMedia error: ', error);
+  }
+  
+  // Initializes media stream.
+  navigator.mediaDevices.getUserMedia(mediaStreamConstraints)
+    .then(gotLocalMediaStream).catch(handleLocalMediaStreamError);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+   
+        <video autoPlay playsInline></video>
+       
     </div>
   );
 }
