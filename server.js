e654d58718578bf56eab0c374f14f2e04cc9214e
deepmal933 <deepmal944@gmail.com>
2020-10-17 18:27:17 -0400

    added peer server and video streaming

--- server.js
+++ server.js
@@ -1,11 +1,32 @@
 const express = require("express")
 const app = express()
-
 const server = require("http").createServer(app);
-const socket = require("socket.io").bind(server)
+const io = require("socket.io")(server)
+const path = require('path');
+const { PeerServer } = require('peer');
+const peerServer = PeerServer({ port: 9000, path: '/myapp' });
+
+
+app.use(express.static(path.join(__dirname, 'build')));
+    
+
+io.on('connection', socket => { 
+
+    socket.on('join-room', (roomId,userId)=>{
+        socket.join(roomId); 
+        console.log(roomId,userId);
+        io.to(roomId).emit("user-connected",userId);
+      });
+
+});
 
 app.use("/",(req,res)=>{
-    res.send("hello world")
+    res.send("");
 })
+
+app.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
 
 server.listen(5000);
